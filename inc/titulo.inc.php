b4c26881ff94f5d82cbf81651158bd946dcc60a9
Troglodyte Developer <troglodev@gmail.com>
2012-09-27 13:19:42 +0200

    Modficiacion sustituyendo switch por array para los titulos

--- inc/titulo.inc.php
+++ inc/titulo.inc.php
@@ -1,31 +1,14 @@
 <?php
 
 function mostrarTitulo($op, $num) {
-    $color = array("blue", "green", "orange", "yellow", "red", "black");
+    $color = array('blue', 'green', 'orange', 'yellow', 'red', 'black');
+    $titulo = array('Entregados: ', 'Avisados: ', 'En Proceso: ',
+        'Esperando Confirmación: ', 'Pendientes: ', 'Todos: ');
 
-    switch ($op) {
-        case 4:
-            $cad = 'Pendientes:';
-            break;
-        case 2:
-            $cad = 'En Proceso:';
-            break;
-        case 3:
-            $cad = 'Esperando Confirmación:';
-            break;
-        case 1:
-            $cad = 'Avisados:';
-            break;
-        case 0:
-            $cad = 'Entregados:';
-            break;
-        default:
-            $cad = 'Todos:';
-    }
 
     echo '<div id="titulo">';
     echo '<a class="' . $color[$op] . '2" href="index.php?c=item&a=showAddItem">';
-    echo '<img src="statico/img/add.png" /><h1>' . $cad . ' ' . $num . '</h1>';
+    echo '<img src="statico/img/add.png" /><h1>' . $titulo[$op] . ' ' . $num . '</h1>';
     echo '</a></div>';
 }
 
