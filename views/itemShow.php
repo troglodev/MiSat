b4de5302079843ebe0e599a781d362dfad75a9f6
Troglodyte Developer <troglodev@gmail.com>
2012-09-27 16:35:46 +0200

    Correcion error <ul><li> y cambios de estilo

--- views/itemShow.php
+++ views/itemShow.php
@@ -5,30 +5,27 @@ mostrarMenu();
 mostrarTitulo($status, count($items));
 
 $color = array("blue", "green", "orange", "yellow", "red", "black");
-echo '<div id="caja"><li>';
+echo '<div id="caja"><ul>';
 $c = 0;
 if ($items != 0) {
     foreach ($items as $item) {
-
-        echo'<ul>';
+        echo'<li>';
         if ($c % 2 == 0) {
             echo '<a style="background:#36363B" class="';
-            echo $color[$item['status']];
-            echo '" href="index.php?c=item&a=showId&id=' . $item['id'] . '">';
         } else {
             echo '<a style="background:#48474C" class="';
-            echo $color[$item['status']];
-            echo '" href="index.php?c=item&a=showId&id=' . $item['id'] . '">';
         }
+        echo $color[$item['status']];
+        echo '" href="index.php?c=item&a=showId&id=' . $item['id'] . '">';
         echo $item['id'] . ' ';
         echo '[' . $item['cat'] . '] ';
         echo $item['desc'];
         echo '</a>';
-        echo '</ul>';
+        echo '</li>';
         $c = $c + 1;
     }
 }
 //echo '</a>';
-echo '</li></div>';
+echo '</ul></div>';
 mostrarPie();
 ?>
